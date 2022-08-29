98707b92ce68ec2c90a6805e7d136b7c515ad67f
Russell Taylor <rctaylorllc@gmail.com>
2022-08-29 14:24:19 -0600

    Add files via upload

--- SQL_Database.sql
+++ SQL_Database.sql
@@ -1,9 +1,14 @@
 -- Creating table for stock analysis
 
-CREATE TABLE Stock_Closing_Price_Date (
-     closing_price INT NOT NULL,
-     closing_date DATE NOT NULL
-    
+CREATE TABLE Stock_Price_Date (
+     
+     Date_ DATE NOT NULL,
+	 Open_ INT NOT NULL,
+	 High_ INT NOT NULL,
+	 Low_ INT NOT NULL,
+     Close_ INT NOT NULL,
+	 Adj_Close_ INT NOT NULL,
+	 Volume_ INT NOT NULL
 );
 
-SELECT * FROM Stock_Closing_Date;
+SELECT * FROM Stock_Price_Date;
